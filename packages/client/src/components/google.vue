181f8fc604d73168fa5078de8558076c89df264d
vib <vib@vibbe.xyz>
2021-10-09 20:18:44 +0300

    change Google search with DuckDuckGo

--- packages/client/src/components/google.vue
+++ packages/client/src/components/google.vue
@@ -15,7 +15,7 @@ const props = defineProps<{
 const query = ref(props.q);
 
 const search = () => {
-	window.open(`https://www.google.com/search?q=${query.value}`, '_blank');
+	window.open(`https://www.duckduckgo.com/?q=${query.value}`, '_blank');
 };
 </script>
 
